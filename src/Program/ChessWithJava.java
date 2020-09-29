d217940f214cdec95321ab9cedf8c2321f5f7e17
RibeiroFilho92 <ribeiro.pdf@hotmail.com>
2020-09-29 18:42:35 -0300

    Command-line logic

--- src/Program/ChessWithJava.java
+++ src/Program/ChessWithJava.java
@@ -3,9 +3,11 @@ package Program;
 
 import BoardGame.BoardNotFinal;
 import BoardGame.Position;
+import Chess.ChessException;
 import Chess.ChessMatch;
 import Chess.ChessPiece;
 import Chess.ChessPosition;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
@@ -17,15 +19,25 @@ public class ChessWithJava {
         ChessMatch chessMatch = new ChessMatch();
         
         while (true) {
-            UserInterface.printBoard(chessMatch.getPieces());
-            System.out.println();
-            System.out.print("Source: ");
-            ChessPosition find = UserInterface.readChessPosition(sc);
-            System.out.println();
-            System.out.print("Target: ");
-            ChessPosition found = UserInterface.readChessPosition(sc);
+            try {
+               UserInterface.clearScreen();
+                UserInterface.printBoard(chessMatch.getPieces());
+                System.out.println();
+                System.out.print("Source: ");
+                ChessPosition find = UserInterface.readChessPosition(sc);
+                System.out.println();
+                System.out.print("Target: ");
+                ChessPosition found = UserInterface.readChessPosition(sc);
+
+                ChessPiece capturedPiece = chessMatch.performChessMove(find, found); 
+            } catch (ChessException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            } catch (InputMismatchException e) {
+                System.out.println(e.getMessage());
+                sc.nextLine();
+            }
             
-            ChessPiece capturedPiece = chessMatch.performChessMove(find, found);
         }
 
     }
